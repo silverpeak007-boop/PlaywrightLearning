eff767764294137137fddf14dee7fbd5b71a4ad7
silverpeak007-boop <silverpeak007@gmail.com>
2026-06-27 15:36:40 +0530

    Added Cherry Pick Demo

--- tests/get-api-validation.spec.ts
+++ tests/get-api-validation.spec.ts
@@ -44,7 +44,7 @@ test('POST API Test', async ({ request }) => {
 
     expect(data.name).toBe('Karthick');
 
-    expect(data.username).toBe('Karthick API');
+    expect(data.username).toBe('Cherry pick demo');
 
 
     console.log('POST API Successful');
